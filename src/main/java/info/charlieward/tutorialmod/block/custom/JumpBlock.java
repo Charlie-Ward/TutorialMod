6fc964439803bbbf07ba2c9d92803f6c404f93f7
charlie-ward <59178341+Charlie-Ward@users.noreply.github.com>
2023-02-22 11:33:39 +0000

    Added tooltips

--- src/main/java/info/charlieward/tutorialmod/block/custom/JumpBlock.java
+++ src/main/java/info/charlieward/tutorialmod/block/custom/JumpBlock.java
@@ -1,5 +1,7 @@
 package info.charlieward.tutorialmod.block.custom;
 
+import net.minecraft.ChatFormatting;
+import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionHand;
@@ -9,10 +11,16 @@ import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class JumpBlock extends Block {
     public JumpBlock(Properties properties) {
@@ -26,6 +34,17 @@ public class JumpBlock extends Block {
             player.sendSystemMessage(Component.literal("Right Clicked this!"));
         }
         return super.use(state, level, pos, player, hand, blockHitResult);
+    }
+
+    @Override
+    public void appendHoverText(ItemStack stack, @Nullable BlockGetter getter, List<Component> components, TooltipFlag flag) {
+        if(Screen.hasShiftDown()){
+            components.add(Component.literal("Stand on this block to get a jump boost").withStyle(ChatFormatting.AQUA));
+        } else {
+            components.add(Component.literal("Press SHIFT for more info").withStyle(ChatFormatting.YELLOW));
+        }
+
+        super.appendHoverText(stack, getter, components, flag);
     }
 
     @Override
