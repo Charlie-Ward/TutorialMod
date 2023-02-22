0be4c8de2d1c0d4be5980195114454de3db9894e
charlie-ward <59178341+Charlie-Ward@users.noreply.github.com>
2023-02-22 10:27:26 +0000

    Added a jump block

--- src/main/java/info/charlieward/tutorialmod/block/ModBlocks.java
+++ src/main/java/info/charlieward/tutorialmod/block/ModBlocks.java
@@ -1,6 +1,7 @@
 package info.charlieward.tutorialmod.block;
 
 import info.charlieward.tutorialmod.TutorialMod;
+import info.charlieward.tutorialmod.block.custom.JumpBlock;
 import info.charlieward.tutorialmod.item.ModCreativeModeTab;
 import info.charlieward.tutorialmod.item.ModItems;
 import net.minecraft.util.valueproviders.UniformInt;
@@ -41,6 +42,10 @@ public class ModBlocks {
     public static final RegistryObject<Block> NETHERRACK_ZIRCON_ORE = registerBlock("netherrack_zircon_ore",
             () -> new DropExperienceBlock(BlockBehaviour.Properties.of(Material.STONE).strength(6f).requiresCorrectToolForDrops(),
                     UniformInt.of(3,7)),
+                    ModCreativeModeTab.TUTORIAL_TAB);
+
+    public static final RegistryObject<Block> JUMP_BLOCK = registerBlock("jump_block",
+            () -> new JumpBlock(BlockBehaviour.Properties.of(Material.STONE).strength(6f).requiresCorrectToolForDrops()),
                     ModCreativeModeTab.TUTORIAL_TAB);
 
     private static <T extends Block>RegistryObject<T> registerBlock(String name, Supplier<T> block, CreativeModeTab tab){
---
+++ src/main/java/info/charlieward/tutorialmod/block/custom/JumpBlock.java
@@ -0,0 +1,38 @@
+package info.charlieward.tutorialmod.block.custom;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.BlockHitResult;
+
+public class JumpBlock extends Block {
+    public JumpBlock(Properties properties) {
+        super(properties);
+    }
+
+    @Override
+    public InteractionResult use(BlockState state, Level level, BlockPos pos, Player player,
+                                 InteractionHand hand, BlockHitResult blockHitResult) {
+        player.sendSystemMessage(Component.literal("Right Clicked this!"));
+
+        return super.use(state, level, pos, player, hand, blockHitResult);
+    }
+
+    @Override
+    public void stepOn(Level level, BlockPos pos, BlockState state, Entity entity) {
+        if(entity instanceof LivingEntity livingEntity){
+            livingEntity.addEffect(new MobEffectInstance(MobEffects.JUMP, 200));
+        }
+
+        super.stepOn(level, pos, state, entity);
+    }
+}
